dee611cf9ff90bc0ea9392f90fcf5ab46be3e756
evinReynaud <ereynaud002@enseirb-matmeca.fr>
2019-03-21 16:07:09 +0100

    Added paths

---
+++ DawImport/XmlPaths.hpp
@@ -0,0 +1,14 @@
+#ifndef XML_PATHS
+#define XML_PATHS
+
+#include <Qstring>
+
+QString PATH_MIDI_TRACKS        = "LiveSet->Tracks->MidiTrack";
+QString PATH_AUDIO_TRACKS       = "LiveSet->Tracks->AudioTrack";
+QString PATH_SLOTS              = "DeviceChain->MainSequencer->ClipSlotList->ClipSlot";
+QString PATH_AUDIO_CLIP_EVENTS  = "DeviceChain->MainSequencer->Sample->ArrangerAutomation->Events->AudioClip";
+QString PATH_MIDI_CLIP_EVENTS   = "DeviceChain->MainSequencer->ClipTimeable->ArrangerAutomation->Events->MidiClip";
+QString PATH_KEY_TRACKS         = "Notes->KeyTracks->KeyTrack";
+QString PATH_NOTES              = "Notes->MidiNoteEvent";
+
+#endif
