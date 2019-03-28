a0086a1f4d412853b11e3bf70df6443a80378576
moi <leo.tisserand.17@gmail.com>
2019-03-28 13:32:05 +0100

    doc for debug

--- DawImport/Debug.hpp
+++ DawImport/Debug.hpp
@@ -4,6 +4,20 @@
 #include "Structs.hpp"
 #include <iostream>
 
+/**
+\file Debug.hpp
+\brief Display every attribute of an AbletonDocument in order to debug
+
+*/
+
+/**
+\fn void displayAbletonDoc(AbletonDocument& doc)
+\brief Display all the fields of the given AbletonDocument and of its underlaying attributes
+
+\param doc The AbletonDocument which has its attributes displayed
+
+*/
+
 void displayAbletonDoc(AbletonDocument& doc);
 
 #endif
